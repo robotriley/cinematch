dda0651f9cfd133530147cd072d9835b00822e79
riley soloner <rileysoloner@gmail.com>
2022-08-22 17:03:31 -0400

    seed fetch req started

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'faraday'
+require 'faraday/net_http'
+Faraday.default_adapter = :net_http
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +8,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "🌱 seeding..."
+
+  Movie.create(title: "", genre: "", top_billed_1: "", top_billed_2: "", top_billed_3: "")
+
+puts "🌱 seeding complete!"
